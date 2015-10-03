6577ed7f1d3c0ab582053eaabb78cac2e64b99bb
meiry <meiry242@gmail.com>
2015-10-03 08:32:49 +0300

    www.gamedevcraft.com SnakeJS v0.1

---
+++ src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+    snakecell_png : "res/snakecell.png",
+    snakefood_png : "res/snakefood.png",
+    blank_png : "res/blank.png"
+   
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
